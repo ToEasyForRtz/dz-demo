c3309defce5218978c5343927e0066db550b96bb
Zaur Kudzoev <plusdale@gmail.com>
2026-02-03 22:33:01 +0300

    5-languages

--- 5-languages/5-languages.js
+++ 5-languages/5-languages.js
@@ -1,13 +1,13 @@
 let greetings = prompt("Write your language"); // пользователь введет текст на своем языке и отталкиваясь от его языка будет соответсвующее приветствие
 
 switch (greetings) {
-  case "German":
+  case "de":
     console.log("Gutten Tag!");
     break;
-  case "English":
+  case "en":
     console.log("Good Morning!");
     break;
-  case "Russian":
+  case "ru":
     console.log("Здравсвтуйте");
     break;
   default:
